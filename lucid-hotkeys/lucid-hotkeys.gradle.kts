4aa77a326427d9d0f038ac76439cb89a4b10a2fe
lucid-soft <lucid-soft@github.com>
2024-02-15 13:23:24 -0500

    - Added a toggle to be able to use the plugin like a bot instead of needing to hit a hotkey to activate. USE WITH CARE.

--- lucid-hotkeys/lucid-hotkeys.gradle.kts
+++ lucid-hotkeys/lucid-hotkeys.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.2.1"
+version = "1.2.2"
 
 project.extra["PluginName"] = "Lucid Hotkeys"
 project.extra["PluginDescription"] = "Setup hotkeys that can do a variety of different actions."
